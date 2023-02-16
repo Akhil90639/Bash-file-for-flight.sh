445fa04579e4bfe060709438bd4d0359d2494536
Akhil <111115830+Akhil90639@users.noreply.github.com>
2023-02-16 10:11:28 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,6 +4,6 @@ sudo apt-get full upgrade -y
 sudo apt-get install python3-pip -y
 sudo apt-get install git -y
 git clone https://github.com/Akhil90639/flight-perdiction.git
-cd flight-perdiction.git
+cd flight-perdiction
 sudo pip3 install -r requirements.txt
 python3 app.py
